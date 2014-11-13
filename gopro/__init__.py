58bf71a08793b56481d45692db0354af31388659
Denis Carriere <carriere.denis@gmail.com>
2014-11-12 22:45:08 -0500

    Change Version

--- gopro/__init__.py
+++ gopro/__init__.py
@@ -14,7 +14,7 @@ GoPro Camera Python module made easy.
 __title__ = 'gopro'
 __author__ = 'Denis Carriere'
 __author_email__ = 'carriere.denis@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2014-2015 Denis Carriere'
 
